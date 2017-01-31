d03f87771b5dae596c4e2918f14e909af07106b0
testTravis <erez.yehezkel@mobiletornado.com>
2017-01-31 14:35:25 +0200

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/erezyehezkel/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/erezyehezkel/myapplication/ExampleUnitTest.java
@@ -14,4 +14,16 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+            @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+        @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 - 2);
+    }
+            @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(5, 2 + 3);
+    }
 }
